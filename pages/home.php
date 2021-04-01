f99b6d6279dc42071014c1345499fa901895e150
captainflyers11@gmail.com <captainflyers11@gmail.com>
2021-04-01 21:40:04 +0200

    GRAPHIKart-GIT

---
+++ pages/home.php
@@ -0,0 +1,31 @@
+
+ <h2> i am on the HOME-PAGE <h2>
+				  
+ <p><a href="index.php?p=single"> PAGE---SINGLE </a></p>
+
+    <ul>
+
+		<?php	
+				
+				$tabdata=$db->query('SELECT * FROM articles','App\Table\article');
+				//var_dump ($tabdata[0]);
+				echo $tabdata[0]-> id;
+				
+				
+				foreach ($db->query('SELECT * FROM articles','App\Table\article') as $post):
+				//var_dump ($post);
+		?>
+				
+				<h2> <a href="<?php echo $post->url ?>"> <?= $post->titre; ?> </a></h2>
+				<p> <?php echo $post->Extrait;
+				?> </p>
+			
+	   <?php endforeach; ?>
+
+   </ul>
+
+
+				  
+ 
+  
+ ;
