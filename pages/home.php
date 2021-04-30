38a814adec0098cd035e9beedafbf339e9847ac8
captainflyers11@gmail.com <captainflyers11@gmail.com>
2021-05-01 00:24:05 +0200

    my-6-commit

--- pages/home.php
+++ pages/home.php
@@ -8,22 +8,22 @@
  
     <ul>
 
-		<?php	
+		    <?php	
 				$tabdata=$db->query('SELECT * FROM articles','App\Table\article');
 				//var_dump ($tabdata[0]);
-				echo $tabdata[0]-> id;
+				echo $tabdata[0]->id;
 				
 				
 				foreach ($db->query('SELECT * FROM articles','App\Table\article') as $post):
 				//var_dump ($post);
-		?>
+		    ?>
 				
 				<h2> <a href="<?php echo $post->url ?>"> <?= $post->titre; ?> </a></h2>
 				<p>
 				    <?php echo $post->Extrait;?> 
 				</p>
 			
-	   <?php endforeach; ?>
+	        <?php endforeach; ?>
 
    </ul>
 
