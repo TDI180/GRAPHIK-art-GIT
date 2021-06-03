46522542783cd305787e0d0fcb0213df97ab8eb9
captainflyers11@gmail.com <captainflyers11@gmail.com>
2021-06-03 13:50:02 +0200

    test-method

--- app/table/test-method.php
+++ app/table/test-method.php
@@ -4,7 +4,37 @@
 	
 	function slug() { echo "zebi";  }
 	
-	$a()=slug();
+	$a=slug();
+	
+	class MyClass
+{
+    // Declare a public constructor
+    public function __construct() { }
+
+    // Declare a public method
+    public function MyPublic() { }
+
+    // Declare a protected method
+    protected function MyProtected() { }
+
+    // Declare a private method
+    private function MyPrivate() { }
+
+    // This is public
+    function Foo()
+    {
+        $this->MyPublic();
+        $this->MyProtected();
+        $this->MyPrivate();
+    }
+}
+
+$myclass = new MyClass;
+
+$myclass->MyPublic(); // Works
+$myclass->MyProtected(); // Fatal Error
+$myclass->MyPrivate(); // Fatal Error
+$myclass->Foo(); // Public, Protected and Private work
 	
 	
 	
